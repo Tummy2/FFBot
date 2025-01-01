cfaf9f8a9eb8d2681482a7f5bfdd4c999d0e5483
chen7756 <chen7756@umn.edu>
2025-01-01 02:16:38 -0600

    presence update

--- main.py
+++ main.py
@@ -4,25 +4,40 @@ import os
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.message_content = True
+intents = discord.Intents.all()
+# intents.message_content = True
 # intents.members = True
-client = discord.Client(intents=intents)
+# intents.presences = True
 
+client = discord.Client(intents=intents)
+guild_status = {}
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
-
+  guild = client.get_guild(1280364300837326868)
+    
+  if guild:
+      for member in guild.members:
+          guild_status[member.id] = ""
+  else:
+      print("Guild not found.")
 @client.event
 async def on_message(message):
-  print(message.author.id)
-
   if message.author == client.user:
     return
   if message.content.startswith('!hello'):
     await message.channel.send('fuck you')
-# @client.event
-# async def on_activity(message):
-  
+
+@client.event
+async def on_presence_update(before, after):
+  new_activity = after.activity.name
+  if guild_status[before.id] == "League of Legends":
+    guild_status[before.id] = new_activity
+
+    return
+  guild_status[before.id] = new_activity
+
+  if(new_activity == "League of Legends"):
+    await client.get_channel(1323920088264609843).send(before.mention + " stop playing league u bum")
 
 client.run(os.getenv('TOKEN'))
